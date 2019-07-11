742ab9a5615b4d93140766061419778c05db32c5
Angela <angela.ohh@gmail.com>
2019-07-11 10:51:50 -0700

    added to git ignore

--- app/src/main/java/com/example/testcore/MainActivity.java
+++ app/src/main/java/com/example/testcore/MainActivity.java
@@ -1,18 +1,29 @@
 // TODO: Connect Firebase and Firestore
+// TODO: MAP OUT WHAT DATA WILL LOOK LIKE
 // TODO: Connect Volley for API Calls
 
 package com.example.testcore;
 
+import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.TextView;
 import android.widget.Toast;
+
+import com.google.android.gms.tasks.OnFailureListener;
+import com.google.android.gms.tasks.OnSuccessListener;
+import com.google.firebase.firestore.DocumentReference;
+import com.google.firebase.firestore.FirebaseFirestore;
+
+import java.util.HashMap;
+import java.util.Map;
 
 public class MainActivity extends AppCompatActivity implements View.OnClickListener {
     private Button signinButton;
@@ -20,6 +31,14 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
     private TextView signinHeader, signinOAuth, showMessages;
 
     private final int REQUEST_CODE = 123;
+
+    // HASHMAP KEYS
+    public static final String KEY_NAME = "name";
+    public static final String KEY_EMAIL = "email";
+
+    // Connection to Firestore
+    private FirebaseFirestore database = FirebaseFirestore.getInstance();
+    private DocumentReference firstDocRef = database.collection("first_storage").document("user");
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -40,10 +59,15 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
     @Override
     public void onClick(View view) {
 
+        String name = signinName.getText().toString().trim();
+        String email = signinEmail.getText().toString().trim();
+
+        addData(name, email);
+
         if (view.getId() == R.id.signin_button) {
 
-            String name = signinName.getText().toString().trim();
-            String email = signinEmail.getText().toString().trim();
+//            String name = signinName.getText().toString().trim();
+//            String email = signinEmail.getText().toString().trim();
 
             showMessages.setText("Sign In Name: " + name + "\n Sign In Email: " + email);
 
@@ -63,5 +87,25 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
             String successMessage = data.getStringExtra("dashboard_callback");
             Toast.makeText(MainActivity.this, successMessage, Toast.LENGTH_LONG).show();
         }
+    }
+
+    private void addData(String name, String email) {
+        Map<String, Object> data = new HashMap<>();
+        data.put(KEY_NAME, name);
+        data.put(KEY_EMAIL, email);
+
+        firstDocRef.set(data)
+                .addOnSuccessListener(new OnSuccessListener<Void>() {
+                    @Override
+                    public void onSuccess(Void aVoid) {
+                        Toast.makeText(MainActivity.this, "Successfully added to Firestore", Toast.LENGTH_LONG).show();
+                    }
+                })
+                .addOnFailureListener(new OnFailureListener() {
+                    @Override
+                    public void onFailure(@NonNull Exception e) {
+                        Log.d("ERROR in Main", "onFailure: " + e.toString());
+                    }
+                });
     }
 }
